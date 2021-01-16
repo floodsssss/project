1a4efd655d5a37ab0c4ce4dbd8b683e4158bdf88
Artem <artemflood@gmail.com>
2021-01-16 23:44:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+"use strict";
+
+//console.log('arr' + " - object");
+//console.log(4 + " - object");
+//console.log(4 +  +"5");
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+console.log(incr);
+console.log(decr);
+
+console.log(--decr);
+console.log(incr++);
+
+console.log(5%2);
+console.log(2*4 ==! 8);
+console.log(2*4 == "8");
+console.log(2*4 ==="8");
+
+const isChecked = true,
+    isClose = false;
+console.log(isChecked || !isClose);
+
+console.log()
+
+let ageArtem = 19,
+    ageSwyat = 23;
+
+--ageArtem;
+++ageSwyat;
+console.log(--ageArtem);
+console.log(ageSwyat);
+console.log(2*4 != 12);
+console.log(2*4 === "12");
+
+
+const Artem = true,
+    Swyat = false;
+console.log(Artem && !Swyat);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const category = "toys";
+
+//console.log(`https://some.com/${category}/5`);
+
+
+
+//const user = "Ivan";
+
+//alert(`Привіт, ${user}`);
+
+
+
+
+//alert('Hello');
+//const result = confirm("Are you here?");
+//console.log(result);
+
+
+
+//const answer = +prompt('Вам є 18?', "18");
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[0] = prompt('Як ви називаєтесь', "");
+//answers[1] = prompt('Як ваше прізвище', "");
+//answers[2] = prompt('Скільки вам років?', "");
+//console.log(typeof(answers));
